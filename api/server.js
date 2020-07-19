0d054f7b2ba1154409e4a26155858ddf68bfb03a
Sunny Gupta <sunny.gupta@primussoft.com>
2020-07-19 12:35:38 +0530

    api deployment

--- api/server.js
+++ api/server.js
@@ -11,43 +11,19 @@ const express = require('express'),
       () => {console.log('Database is connected') },
       err => { console.log('Can not connect to the database'+ err)}
     );
+// var morgan = require('morgan');
 
     const app = express();
     app.use(bodyParser.json());
+    // app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
     app.use(cors());
+// app.use(morgan('dev'));
+
     app.use('/business', businessRoute);
     const port =  4000;
 
     const server = app.listen(port, function(){
      console.log('Listening on  ' + port);
     });
-
-    
-//     var express = require('express');
-// var bodyParser = require('body-parser');
-// var cors = require('cors');
-// var morgan = require('morgan');
-
-// var app = express();
-// var mongoose = require('mongoose');
-
-// mongoose.Promise = global.Promise;
-
-// mongoose.connect('mongodb://localhost/gtm');
-
-// app.use(cors());
-// app.use(morgan('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-
-// var cardRoutes = require('./routes/card.routes.js')(app);
-// var columnRoutes = require('./routes/column.routes.js')(app);
-// var boardRoutes = require('./routes/board.routes.js')(app);
-
-// var server = app.listen(3001, function () {
-//     console.log('Server running at http://127.0.0.1:3001/');
-// });
-
-// var io = require('socket.io').listen(server);
