e622f67a8b4cbac4239a6aa727d9f9f9fbd0ae0d
Sunny Gupta <sunny.gupta@primussoft.com>
2020-08-11 21:01:40 +0530

    nodemodiles remopved

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ const express = require('express'),
 
     app.use('/business', businessRoute);
 
-    const port = process.env.port;
+    const port = process.env.port || 4000;
 
     const server = app.listen(port, function(){
      console.log('Listening on  ' + port);
